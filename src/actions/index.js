16b0dcc13e1388dabeac8b9ff5100d93b85523cc
LeChristopher Blackwell <codeblackwell@gmail.comgit status\curl -sSL https://get.rvm.io | bash -s stablegit config --global user.email codeblackwell@gmail.com>
2016-08-16 18:50:35 -0700

    setup template strings in ES6 for axios to call weather API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,21 @@
-const API_KEY = '4f39976db222378291e2b24458625068';
+import axios from'axios'
 
+
+/**ES6 Template strings
+* use  `` (backtick) to template a string with variables. Fancy Concatenation
+*/
+
+const API_KEY = '4f39976db222378291e2b24458625068';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}` 
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url);
+
+
+  return {
+    type: FETCH_WEATHER
+  }
+}
